bf7de39d8ef9e07a095f827738035bc3d8ab2177
pau2017 <pau.higon.2017@lacetania.cat>
2021-03-29 17:18:16 +0200

    Cruilla acabada sense botons

--- Cruilla_de_semaforts/Cruilla_de_semaforts.ino
+++ Cruilla_de_semaforts/Cruilla_de_semaforts.ino
@@ -10,6 +10,8 @@ unsigned long TEMPO6=15000;
 unsigned long TEMPO7=20000;
 unsigned long TEMPO8=21000;
 unsigned long TEMPOPA=0;
+unsigned long TEMPOSPA=
+unsigned long TEMPOSPB=
 unsigned long TEMPOPB=0;
 int bp1 = 17;                             //boto peatons Semafor A
 int bp2 = 18;                             //boto peatons semafor B
@@ -20,49 +22,86 @@ int sortides= 9;
 void setup() {
   
    Serial.begin(9600);
-   for(int i = 0; i < ledNum; i++){
+   for (int i = 0; i < ledNum; i++){
     
    pinMode(ledPin[i], OUTPUT);
    }
-   for(int j = 0; j < ledNum ; j++){
+   for (int j = 0; j < ledNum ; j++){
 
    digitalWrite(ledPin[j], 0);
+   ;
    }
    pinMode(bp1, INPUT);
    pinMode(bp2, INPUT);
+ 
 }
 
 void loop() {
 //*******************************SEMAFOR A*******************************************************
 
-  if (millis ()- T > TEMPO5){                            
+  if (millis() - T < TEMPO5){                            
     
     digitalWrite(7,1);
     digitalWrite(8,0);
     digitalWrite(9,0);
+   
   }
-  else if (millis () - T > TEMPO5 && millis () T < TEMPO6){         //Semafor A Verd
+  else if (millis() - T > TEMPO5 && millis () - T < TEMPO6){         //Semafor A Verd
 
      digitalWrite(7,0);
      digitalWrite(8,0);
      digitalWrite(9,1);
+   
   }
-  else if (millis () - T > TEMPO6 && T millis () - TEMPO7){         //Semafort A turonja
+  else if (millis() - T < TEMPO7 && millis () - T > TEMPO6){         //Semafort A turonja
 
      digitalWrite(7,0);
      digitalWrite(8,1);
      digitalWrite(9,0);
   }
-  else if (millis () -T > TEMPO7 && millis () -T - TEMPO8){         //semafort A vermell
+  else if (millis() - T < TEMPO8 && millis () - T > TEMPO7){         //semafort A vermell
 
     digitalWrite(7,1);
     digitalWrite(8,0);
     digitalWrite(9,0);
   }
+  else if (millis() - T >= TEMPO8){
+    T = millis();
+    
+  }
   
 //*******************************SEMAFOR B****************************************************
 
-  if (millis () 
+  if (millis() - T < TEMPO1){                                     //semafort B Vermell
+
+    
+    digitalWrite(10,1);
+    digitalWrite(11,0);
+    digitalWrite(12,0);
+  }
+  else if (millis() - T > TEMPO1 && millis() - T < TEMPO3){         //semafort B Verd
+    digitalWrite(10,0);
+    digitalWrite(11,0);
+    digitalWrite(12,1);
+  }
+  else if (millis() - T > TEMPO3 && millis() - T < TEMPO4){          //semafort B Turonja
+
+    digitalWrite(10,0);
+    digitalWrite(11,1);
+    digitalWrite(12,0);
+  }
+  else if (millis() - T > TEMPO4 && millis() - T < TEMPO8){         //semafort B Vermell
+    digitalWrite(10,1);
+    digitalWrite(11,0);
+    digitalWrite(12,0);
+  }
+
+
+
+
+
+
+
   
   //if (millis()-ARon <= ARtimon){
     //digitalWrite(7,HIGH);                                                             // Semafor A vermell 

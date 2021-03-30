d9bed2e4d8338cd7f5450f3b46379c5e4c53028d
pau2017 <pau.higon.2017@lacetania.cat>
2021-03-30 14:17:59 +0200

    test

--- Cruilla_de_semaforts/Cruilla_de_semaforts.ino
+++ Cruilla_de_semaforts/Cruilla_de_semaforts.ino
@@ -1,6 +1,8 @@
 const int ledPin[]={7,8,9,10,11,12,13,14,15,16};
 int ledNum=10;
 unsigned long T=0;                        //Sincronització amb millis per comparar
+unsigned long Ts1=0;                      //Sincronitzacio amb millis per comparar semafor A
+unsigned long Ts2=0;                      //Sincronitzacio amb millis per comparar semafor B
 unsigned long TEMPO1=1000;
 unsigned long TEMPO2=4000;
 unsigned long TEMPO3=5000;
@@ -10,11 +12,13 @@ unsigned long TEMPO6=15000;
 unsigned long TEMPO7=20000;
 unsigned long TEMPO8=21000;
 unsigned long TEMPOPA=0;
-unsigned long TEMPOSPA=
-unsigned long TEMPOSPB=
 unsigned long TEMPOPB=0;
+unsigned long TEMPOSEMM=2000;
+unsigned long TEMPOSEM=5000;
+bool intermitenciaa = true;
+bool intermitenciab = true;
 int bp1 = 17;                             //boto peatons Semafor A
-int bp2 = 18;                             //boto peatons semafor B
+int bp2 = 22;                             //boto peatons semafor B
 int bp1s = 0;
 int bp2s = 0;
 int sortides= 9;
@@ -37,33 +41,44 @@ void setup() {
 }
 
 void loop() {
-//*******************************SEMAFOR A*******************************************************
 
-  if (millis() - T < TEMPO5){                            
+
+  
+//*******************************SEMAFOR A*******************************************************
+ 
+  if (millis() - T < TEMPO5 && bp1s == 0){                            
     
     digitalWrite(7,1);
     digitalWrite(8,0);
     digitalWrite(9,0);
+    digitalWrite(13,1);
+    digitalWrite(14,0);
    
   }
-  else if (millis() - T > TEMPO5 && millis () - T < TEMPO6){         //Semafor A Verd
+  else if (millis() - T > TEMPO5 && millis () - T < TEMPO6 && bp1s == 0){         //Semafor A Verd
 
      digitalWrite(7,0);
      digitalWrite(8,0);
      digitalWrite(9,1);
+     digitalWrite(13,1);
+     digitalWrite(14,0);
    
   }
-  else if (millis() - T < TEMPO7 && millis () - T > TEMPO6){         //Semafort A turonja
+  else if (millis() - T < TEMPO7 && millis () - T > TEMPO6 && bp1s == 0){         //Semafort A turonja
 
      digitalWrite(7,0);
      digitalWrite(8,1);
      digitalWrite(9,0);
+     digitalWrite(13,1);
+     digitalWrite(14,0);
   }
-  else if (millis() - T < TEMPO8 && millis () - T > TEMPO7){         //semafort A vermell
+  else if (millis() - T < TEMPO8 && millis () - T > TEMPO7 && bp1s == 0){         //semafort A vermell
 
     digitalWrite(7,1);
     digitalWrite(8,0);
     digitalWrite(9,0);
+    digitalWrite(13,1);
+    digitalWrite(14,0);
   }
   else if (millis() - T >= TEMPO8){
     T = millis();
@@ -72,28 +87,38 @@ void loop() {
   
 //*******************************SEMAFOR B****************************************************
 
-  if (millis() - T < TEMPO1){                                     //semafort B Vermell
+  if (millis() - T < TEMPO1 && bp2s == 0){                                     //semafort B Vermell
 
     
     digitalWrite(10,1);
     digitalWrite(11,0);
     digitalWrite(12,0);
+    digitalWrite(15,1);
+    digitalWrite(16,0);
   }
-  else if (millis() - T > TEMPO1 && millis() - T < TEMPO3){         //semafort B Verd
+  else if (millis() - T > TEMPO1 && millis() - T < TEMPO3 && bp2s == 0){         //semafort B Verd
+    
     digitalWrite(10,0);
     digitalWrite(11,0);
     digitalWrite(12,1);
+    digitalWrite(15,1);
+    digitalWrite(16,0);
   }
-  else if (millis() - T > TEMPO3 && millis() - T < TEMPO4){          //semafort B Turonja
+  else if (millis() - T > TEMPO3 && millis() - T < TEMPO4 && bp2s == 0){          //semafort B Turonja
 
     digitalWrite(10,0);
     digitalWrite(11,1);
     digitalWrite(12,0);
+    digitalWrite(15,1);
+    digitalWrite(16,0);
   }
-  else if (millis() - T > TEMPO4 && millis() - T < TEMPO8){         //semafort B Vermell
+  else if (millis() - T > TEMPO4 && millis() - T < TEMPO8 && bp2s == 0){         //semafort B Vermell
+    
     digitalWrite(10,1);
     digitalWrite(11,0);
     digitalWrite(12,0);
+    digitalWrite(15,1);
+    digitalWrite(16,0);
   }
 
 
